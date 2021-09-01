0513a373f7c39e312bc0800ccfae673ad09d57e7
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2021-09-01 12:39:53 +0200

    Add documentation and cleanup code

--- indicators.py
+++ indicators.py
@@ -50,9 +50,9 @@ class RandomFlag(Indicator):
                 ]
                 return out
 
-class Noise(Indicator):
+class Corona(Indicator):
         '''
-        Example of Noise heatmap indicator for points centered in each grid cell.
+        An example Corona indicator which displays heat maps in different locations depending on the amount of offices.
 
         Note that this class requires the geometry of the table as input, which is why it sets:
         requires_geometry = True
@@ -62,63 +62,40 @@ class Noise(Indicator):
         def setup(self):
                 self.indicator_type = 'heatmap'
                 self.requires_geometry = True
-                self.name = "hot"
+                self.name = "corona"
         def load_module(self):
                 self.parks = []
 
-
         def return_indicator(self, geogrid_data):
                 features = []
-                
-                # print(geogrid_data) 
+
+                # Count the amount of offices and store their locations
                 offices = 0
                 office_points = []
                 for i, cell in enumerate(geogrid_data):
                         name = cell["name"]
                         if name == "Office":
-                                offices+=1
+                                offices += 1
                                 lat,lon = zip(*cell['geometry']['coordinates'][0])
                                 lat,lon = mean(lat),mean(lon)
                                 office_points.append((lat, lon))
-                d = pd.read_csv("Parks_cityscope.csv")
-                # for index, row in d.iterrows():
-                # print(row['x'], row['y'])
-                parks = list(zip(d["x"].values.tolist(), d["y"].values.tolist()))
-                # for i, cell in enumerate(geogrid_data):
 
-                #         name = cell["name"]
-                #         # print(cell)
-                #         # if name != "Institutional":
-                #                 # continue
-                #         # if i % 2 == 0: 
-                #         #         continue
-                #         feature = {}
-                #         lat,lon = zip(*cell['geometry']['coordinates'][0])
-                #         z = zip(*cell['geometry']['coordinates'][0])
-                #         # print(z)
-                       
-                #         # print(lat)
-                #         lat,lon = mean(lat),mean(lon)
-                #         # lat += 0.001
-                #         # feature['geometry'] = {'coordinates': [ [4.468920000000006, 51.924213999992006],[4.46891999793304, 51.92377142238439], [4.4696358074567515, 51.92377141924936], [4.469635815530895, 51.924213996856935],[4.468920000000006, 51.924213999992006]],'type': 'Polygon'}
-                #         # feature['geometry'] = {'coordinates': [lat,lon],'type': 'Point'}
-                #         # feature['properties'] = {self.name:random()}
-                #         # feature['properties'] = {self.name: random.randint(0, 20)}
-                #         print(offices)
-                        # test = 10
-                # if (offices < 6 and name == "Park") or (offices >= 6 and name == "Office") : 
-                        # print("doing something")
+                # Load the locations of the parks
+                d = pd.read_csv("Parks_cityscope.csv")
+                parks = list(zip(d["x"].values.tolist(), d["y"].values.tolist()))
+
                 heat_points = parks
                 if offices >= 8:
                         heat_points = office_points
                 
+                # Set the heat maps to the right locations depending on the amount of offices
                 for x, y in heat_points:
                         feature={}
                         feature['geometry'] = {'coordinates': [x,y],'type': 'Point'}
                         feature['properties'] = {self.name: random.randint(0, 20)}
                         #         feature['properties'] = {self.name: test, "whatever": 6}
-                        #         feature['properties'] = {self.name: test, "whatever": 6}
                         features.append(feature)
+
                 out = {'type':'FeatureCollection','features':features}
                 return out
 
