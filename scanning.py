9059053f8d13ea90cdf74c443915d6dff45a61b8
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2021-09-01 12:39:53 +0200

    Add documentation and cleanup code

--- scanning.py
+++ scanning.py
@@ -4,6 +4,7 @@ import json
 import os
 import hashlib
 
+# Predefined mapping of scanned Lego types to building types
 types = {
     -1:"Invalid", 
     0:"Institutional", 
@@ -15,27 +16,29 @@ types = {
 H = Handler('yourtest',quietly=False)
 geogrid_data = H.get_geogrid_data()
 
-scanned = [[0, 0], [1, 0], [1, 0], [1, 0], [1, 0], [-1, -1], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0]]
-
 def perform_update():       
         H.update_geogrid_data(update_types)
 def update_types(geogrid_data):
+        scanned = [-1, -1]
         with open('CS_CityScoPy/scanner_data.txt') as json_file:
-                data = json.load(json_file)
-        scanned = data
+                scanned = json.load(json_file)
        
         print(f"Updating grid to \n{scanned}")
+        
+        # Make sure the data is in the right format
         if not type(scanned[0]) == list:
                 return geogrid_data
-        # print(geogrid_data)
+
+        # Update the table cell types in the server to the scanned types
         for i, cell in enumerate(geogrid_data):
+                # To facilitate testing, make sure only the first cell in the table is updated
                 if i >= 1: 
                         continue
-                cell['name'] = 'Institutional'
-                id = cell["id"]
 
                 if scanned[i][0] not in types:
                         continue
+
+                id = cell["id"]
                 scanned_type = types[scanned[i][0]]
                 if scanned_type != "Invalid":
                         cell['name'] = scanned_type
@@ -43,38 +46,18 @@ def update_types(geogrid_data):
         return geogrid_data
 
 
-def periodic_work(interval):
+def scan_for_updates(interval):
     oldHash = ""
     while True:
-        #change this to the function you want to call, or paste in the code you want to run
-        monkey = Monkey()
-        # monkey.ook()
+        # Update table whenever scanner_data.txt is modified
         with open('CS_CityScoPy/scanner_data.txt', "rb") as f:
                 newHash = hashlib.md5(f.read()).hexdigest()
                 if oldHash != newHash:
                         perform_update() 
                         oldHash = newHash
     
-        # perform_update()
         #interval should be an integer, the number of seconds to wait
         time.sleep(interval) 
 
-
-class Monkey(object):
-    def __init__(self):
-        self._cached_stamp = 0
-        self.filename = 'CS_CityScoPy/scanner_data.txt'
-
-    def ook(self):
-        stamp = os.stat(self.filename).st_mtime
-        print("old stamp ", stamp)
-        if stamp != self._cached_stamp:
-            self._cached_stamp = stamp
-            
-            print("they are different")
-            # File has changed, so do something
-            perform_update()
-        else:
-            print("no update")
 if __name__ == '__main__':
-    periodic_work(2)
+    scan_for_updates(2)
