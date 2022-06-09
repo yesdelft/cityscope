7e6eb6f5c96af0a7e95106a348db65c56866fcc5
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2022-06-09 12:29:50 +0200

    Heavily optimize conversion to datetime and extract data simulation code

--- CS_cityscopeJS/src/data/smart.py
+++ CS_cityscopeJS/src/data/smart.py
@@ -1,23 +1,20 @@
 import pandas as pd
 class Smart:
     def __init__(self):
-        self.energy = pd.read_csv("EUR/Integrated/energy_usages.csv", converters= {"time": pd.to_datetime}, index_col=0) 
-        self.energy["month"] = pd.DatetimeIndex(self.energy.index).month
-        self.energy["year"] = pd.DatetimeIndex(self.energy.index).year
+        def convert_datetime(series):
+            return pd.to_datetime(series, format="%Y-%m-%d %H:%M:%S")
+        self.energy = pd.read_csv("EUR/Integrated/energy_usages.csv",  parse_dates=["time"], date_parser=convert_datetime, index_col=0)
+        # self.energy["month"] = pd.DatetimeIndex(self.energy.index).month
+        # self.energy["year"] = pd.DatetimeIndex(self.energy.index).year
 
 
     def monthlySums(self, year):
         year_energy = self.energy[self.energy["year"]==year]
-        # monthly_sums = self.energy.groupby(pd.Grouper(level='time', freq='M')).sum()
-        # year_energy["result"]
         monthly_sums = year_energy.groupby([pd.Grouper(level='time', freq='M'),'building_name'])["kWh"].sum().reset_index()
-        # monthly_sums.set_index("time")
         monthly_sums["month"] = pd.DatetimeIndex(monthly_sums["time"]).month
-        return monthly_sums[["month", "building_name", "kWh"]]
-
-        
+        return monthly_sums[["month", "building_name", "kWh"]]        
     
 if __name__ == "__main__":
     smart = Smart()
 
-    print(smart.monthlySums(2019))
+    # print(smart.monthlySums(2019))
